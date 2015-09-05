e17db7aab111840e8797ae284b335e97250bdf45
Alvi Sait <asait@192-168-1-100.tpgi.com.au>
2015-09-05 23:16:57 +1000

    Changed to ViewController

--- SearchBarHeader/SearchBarHeader/ViewController.swift
+++ SearchBarHeader/SearchBarHeader/ViewController.swift
@@ -10,16 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var searchContainerView: UIView!
+
+  private var headerView: HeaderView!
+  private var refreshControl: UIRefreshControl!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    self.automaticallyAdjustsScrollViewInsets = false
+
+    headerView = NSBundle.mainBundle().loadNibNamed("HeaderView", owner: self, options: nil)[0] as? HeaderView
+    headerView.frame = CGRectMake(0, 0, CGRectGetWidth(self.tableView.frame), 144)
+
+    let headerContainerView = UIView(frame: headerView.frame)
+    headerContainerView.backgroundColor = UIColor.clearColor()
+    headerContainerView.addSubview(headerView)
+    headerView.clipsToBounds = false
+
+    self.tableView.tableHeaderView = headerContainerView
+
+    self.refreshControl = UIRefreshControl()
+    self.refreshControl?.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+    self.tableView.addSubview(refreshControl)
+
+    refreshControl?.bounds = CGRectMake(
+      refreshControl!.bounds.origin.x,
+      -CGRectGetHeight(headerView.frame),
+      refreshControl!.bounds.size.width,
+      refreshControl!.bounds.size.height)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func refresh() {
+    sleep(3)
+
+    if self.refreshControl!.refreshing {
+      self.refreshControl?.endRefreshing()
+    }
+  }
+
+}
+
+extension ViewController: UITableViewDelegate {
+  func scrollViewDidScroll(scrollView: UIScrollView) {
+    var offset = scrollView.contentOffset.y
+
+    if let headerView = self.headerView {
+      let maximumHeaderOffset = CGRectGetHeight(headerView.frame) - CGRectGetHeight(headerView.searchBar.frame)
+      if offset >= maximumHeaderOffset {
+//        headerView.transform = CGAffineTransformMakeTranslation(0, offset - maximumHeaderOffset)
+        if let searchBar = self.headerView.searchBar {
+          if searchBar.superview == self.headerView {
+            searchBar.removeFromSuperview()
+            searchContainerView.addSubview(searchBar)
+            searchBar.frame = CGRectMake(0, 0, CGRectGetWidth(searchContainerView.frame), CGRectGetHeight(searchContainerView.frame))
+          }
+        }
+      } else if (offset <= 0){
+        headerView.transform = CGAffineTransformMakeTranslation(0, min(offset, 0))
+      }
+    }
+  }
+}
+
+
+extension ViewController: UITableViewDataSource {
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 100
   }
 
 
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = "CELL \(indexPath.row)"
+    return cell
+  }
 }
 
